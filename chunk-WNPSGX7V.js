b9104ca4ff88706d353990a7d235c6d0ddf2648e
BrDev <bmontero912@gmail.com>
2024-06-01 18:34:35 -0500

    Auto-generated commit

---
+++ chunk-WNPSGX7V.js
@@ -0,0 +1,4 @@
+import{Fa as e,Ga as t,Ha as l,Ma as n,Na as m,W as a}from"./chunk-ZFLGCVVU.js";var p=(()=>{let i=class i{};i.\u0275fac=function(r){return new(r||i)},i.\u0275cmp=a({type:i,selectors:[["app-page-home"]],standalone:!0,features:[m],decls:15,vars:0,consts:[[1,"h-52"],[1,"c-btn1"],[1,"bg-o","h-52"],[1,"border","group","hover:text-red-500"],["href","#",1,"text-blue-500"]],template:function(r,s){r&1&&(e(0,"div",0)(1,"h1"),n(2,"INICIO"),t(),l(3,"br"),e(4,"button",1),n(5," Boton S1 "),t()(),e(6,"div",2),n(7,` Elm 2
+`),t(),e(8,"div",3)(9,"a",4),n(10,"Enlace"),t()(),e(11,"div",0),n(12,` Elm 3
+`),t(),e(13,"div",2),n(14,` Elm 4
+`),t())}});let o=i;return o})();export{p as default};
