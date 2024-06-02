dc2fb02b1eb380f9f2286607a89f3aa56afa2e02
BrDev <bmontero912@gmail.com>
2024-06-02 03:55:39 -0500

    Auto-generated commit

--- chunk-G5EAKHA6.js
+++
@@ -1,4 +0,0 @@
-import{Fa as t,Ga as n,Ha as m,Ma as i,Na as r,W as l}from"./chunk-ZFLGCVVU.js";var p=(()=>{let e=class e{};e.\u0275fac=function(o){return new(o||e)},e.\u0275cmp=l({type:e,selectors:[["app-page-services"]],standalone:!0,features:[r],decls:12,vars:0,consts:[[1,"h-52"],[1,"c-btn1"],[1,"bg-o","h-52"]],template:function(o,s){o&1&&(t(0,"div",0)(1,"h1"),i(2,"SERVICIOS"),n(),m(3,"br"),t(4,"button",1),i(5," Boton S1 "),n()(),t(6,"div",2),i(7,` Elm 2
-`),n(),t(8,"div",0),i(9,` Elm 3
-`),n(),t(10,"div",2),i(11,` Elm 4
-`),n())}});let a=e;return a})();export{p as default};
