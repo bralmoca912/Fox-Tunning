faef3403b0439cb59099c1ece4f7757336dd0bdb
BrDev <bmontero912@gmail.com>
2024-06-02 03:54:17 -0500

    Íconos con FontAwasome, for para las rutas y update del routerLinkActive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,22 +5,18 @@ import PageServicesComponent from './dashboard/page-services/page-services.compo
 export const routes: Routes = [
     {
         path: 'page-fav',
-        title: 'Favoritos',
         loadComponent: () => import('./dashboard/page-fav/page-fav.component'),
     },
     {
         path: 'page-home',
-        title: 'Inicio',
         loadComponent: () => import('./dashboard/page-home/page-home.component'),
     },
     {
         path: 'page-nosotros',
-        title: 'Nosotros',
         loadComponent: () => import('./dashboard/page-nosotros/page-nosotros.component'),
     },
     {
         path: 'page-services',
-        title: 'Servicios',
         loadComponent: () => import('./dashboard/page-services/page-services.component'),
     },
     { path: '', redirectTo: '/page-home', pathMatch: 'full' },
--- src/app/shared/top-menu-bar/top-menu-bar.component.ts
+++ src/app/shared/top-menu-bar/top-menu-bar.component.ts
@@ -12,15 +12,17 @@ import { Router, RouterLink, RouterModule } from '@angular/router';
 export class TopMenuBarComponent {
 
   anchoDeTrazo: number = 1.5; // Valor inicial del ancho del trazo
-
-  cambiarAncho() {
-    this.anchoDeTrazo = this.anchoDeTrazo === 1.5 ? 2.5 : 1.5; // Cambiar el valor del ancho del trazo
-  }
-
   constructor(private router: Router) { }
 
-  isActive(route: string): boolean {
+  cambiarAncho() { // Pasar el ancho de 1.5 a 2.5
+    this.anchoDeTrazo = this.anchoDeTrazo === 1.5 ? 2.5 : 1.5;
+  }
+  isActive(route: string): boolean { // verificamos si la ruta actual es activa o no
     return this.router.isActive(route, true);
   }
-
+  items = [ // Array de rutas
+    { title: 'Inicio', route: '/page-home', icon: 'fas fa-home' },
+    { title: 'Servicios', route: '/page-services', icon: 'fa-solid fa-folder-open' },
+    { title: 'Nosotros', route: '/page-nosotros', icon: 'fa-solid fa-users' },
+  ];
 }
