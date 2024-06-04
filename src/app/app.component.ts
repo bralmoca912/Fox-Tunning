1b0aabffd980470d96299b8ae5417483dfe6c55b
BrDev <bmontero912@gmail.com>
2024-06-03 19:58:16 -0500

    Footer Funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopMenuBarComponent } from './shared/top-menu-bar/top-menu-bar.component';
+import FooterComponent from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopMenuBarComponent],
+  imports: [RouterOutlet, TopMenuBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
