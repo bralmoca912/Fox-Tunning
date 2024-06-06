448b48863de741a983c4a28b15a2f164643c3e9a
BrDev <bmontero912@gmail.com>
2024-06-05 19:37:03 -0500

    Auto-generated commit

--- chunk-JME7CJWA.js
+++
@@ -1,6 +0,0 @@
-import{Ja as e,Ka as t,La as a,Sa as n,Ta as r,W as o}from"./chunk-UA6ZDMK3.js";var p=(()=>{let i=class i{};i.\u0275fac=function(m){return new(m||i)},i.\u0275cmp=o({type:i,selectors:[["app-page-nosotros"]],standalone:!0,features:[r],decls:16,vars:0,consts:[[1,"h-52"],[1,"c-btn1"],[1,"bg-o","h-52"]],template:function(m,s){m&1&&(e(0,"div",0)(1,"h1"),n(2,"NOSOTROS"),t(),a(3,"br"),e(4,"button",1),n(5," Boton S1 "),t()(),e(6,"div",2),n(7,` Elm 2
-`),t(),e(8,"div",0),n(9,` Elm 3
-`),t(),e(10,"div",2),n(11,` Elm 4
-`),t(),e(12,"div",0),n(13,` Elm 5
-`),t(),e(14,"div",2),n(15,` Elm 6
-`),t())}});let l=i;return l})();export{p as default};
