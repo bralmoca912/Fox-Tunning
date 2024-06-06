cac27a16d090fa6f8975ab366f1226edefdc50dd
BrDev <bmontero912@gmail.com>
2024-06-06 04:04:56 -0500

    Mensaje de alerta en page-home

--- src/app/dashboard/page-home/page-home.component.ts
+++ src/app/dashboard/page-home/page-home.component.ts
@@ -1,13 +1,23 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-page-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './page-home.component.html',
   styleUrl: './page-home.component.css'
 })
 export default class PageHomeComponent {
+
+  showAlert: boolean = true;
+
+  ngOnInit(): void {
+    // Mostrar la alerta durante 5 segundos
+    setTimeout(() => {
+      this.showAlert = false; // Ocultar la alerta después de 5 segundos
+    }, 10000);
+  }
 
 }
