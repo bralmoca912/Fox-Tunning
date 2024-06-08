9e9836d0701c2d4ef3b1f80bbfeab2f8a3064c9e
BrDev <bmontero912@gmail.com>
2024-06-07 21:31:35 -0500

    page-services prototipo 1 finalizado

--- src/app/dashboard/page-services/page-services.component.ts
+++ src/app/dashboard/page-services/page-services.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-page-services',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './page-services.component.html',
   styleUrl: './page-services.component.css'
 })
